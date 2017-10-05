5910209a497cc164681f9fe457990571db213703
ddialar <ddialar@gmail.com>
2017-10-05 03:11:39 +0100

    chore(paso-5): The starting point is working successfully.

--- src/graphql/models/director.type.js
+++ src/graphql/models/director.type.js
@@ -1,37 +1,26 @@
 import {
     GraphQLObjectType,
     GraphQLInt,
-    GraphQLString
+    GraphQLString,
+    GraphQLList
 } from 'graphql';
+
+import * as MoviesDirectorsService from '../services/movies-directors.service';
 
 var DirectorType = new GraphQLObjectType({
     name: 'Director',
     fields: () => ({
         id: { type: GraphQLInt },
-        name: { type: GraphQLString }
+        name: { type: GraphQLString },
+        movies: {
+            type: new GraphQLList(MovieType),
+            resolve: async (parentValues, args) => {
+                return await MoviesDirectorsService.getMoviesDataByDirectorId(parentValues.id);
+            }
+        }
     })
 });
 
 export default DirectorType;
 
-/*
-Solcuión Tarea 1
-import * as MoviesDirectorsService from '../services/movies-directors.service';
-
-Solcuión Tarea 2
 import MovieType from './movie.type';
-
-Solcuión Tarea 3
-movies: {
-    type: new GraphQLList(MovieType),
-    resolve: async (parentValues, args) => {
-        console.log(`parentValues: ${JSON.stringify(parentValues)`);
-        return [];
-    }
-}
-
-Solcuión Tarea 4
-resolve: async (parentValues, args) => {
-    return await MoviesDirectorsService.getMoviesDataByDirectorId(parentValues.id);
-}
-*/
--- src/graphql/models/movie.type.js
+++ src/graphql/models/movie.type.js
@@ -5,6 +5,12 @@ import {
     GraphQLString
 } from 'graphql';
 
+// Tarea 1.a - Importar el servicio 'MoviesDirectorsService'.
+// Tarea 1.b - Importa el objeto tipo 'DirectorType' después de 'export default'.
+
+// Tarea 2.a - Importar el servicio 'MoviesActorsService'.
+// Tarea 2.b - Importa el objeto tipo 'ActorType' después de 'export default'.
+
 var MovieType = new GraphQLObjectType({
     name: 'Movie',
     fields: () => ({
@@ -16,7 +22,38 @@ var MovieType = new GraphQLObjectType({
         rating: { type: GraphQLFloat },
         classification: { type: GraphQLString },
         year: { type: GraphQLString }
+        // Tarea 3 - Añadir el campo 'directors' y devolver la información correspondiente.
+        // Tarea 4 - Añadir el campo 'actors' y devolver la información correspondiente.
     })
 });
 
 export default MovieType;
+
+
+/*
+Solución Tarea 1.a
+import * as MoviesDirectorsService from '../services/movies-directors.service';
+Solución Tarea 1.b
+import DirectorType from './director.type';
+
+Solución Tarea 2.a
+import * as MoviesActorsService from '../services/movies-actors.service';
+Solución Tarea 2.b
+import ActorType from './actor.type';
+
+Solución Tarea 3
+directors: {
+    type: new GraphQLList(DirectorType),
+    resolve: async (parentValues, args) => {
+        return await MoviesDirectorsService.getDirectorsDataByMovieId(parentValues.id);
+    }
+},
+
+Solución Tarea 4
+actors: {
+    type: new GraphQLList(ActorType),
+    resolve: async (parentValues, args) => {
+        return await MoviesActorsService.getActorsDataByMovieId(parentValues.id);
+    }
+},
+*/
