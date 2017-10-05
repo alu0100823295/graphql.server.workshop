884f644745fa14b559a184bbefbf30b10f926a83
ddialar <ddialar@gmail.com>
2017-10-05 03:21:15 +0100

    chore(paso-5): Preparate the code in order to implement the mutations.

--- src/graphql/models/movie.type.js
+++ src/graphql/models/movie.type.js
@@ -2,14 +2,14 @@ import {
     GraphQLObjectType,
     GraphQLInt,
     GraphQLFloat,
-    GraphQLString
+    GraphQLString,
+    GraphQLList
 } from 'graphql';
 
-// Tarea 1.a - Importar el servicio 'MoviesDirectorsService'.
-// Tarea 1.b - Importa el objeto tipo 'DirectorType' después de 'export default'.
-
-// Tarea 2.a - Importar el servicio 'MoviesActorsService'.
-// Tarea 2.b - Importa el objeto tipo 'ActorType' después de 'export default'.
+import * as MoviesDirectorsService from '../services/movies-directors.service';
+import * as MoviesWritersService from '../services/movies-writers.service';
+import * as MoviesActorsService from '../services/movies-actors.service';
+import * as MoviesGenresService from '../services/movies-genres.service';
 
 var MovieType = new GraphQLObjectType({
     name: 'Movie',
@@ -21,39 +21,44 @@ var MovieType = new GraphQLObjectType({
         duration: { type: GraphQLInt },
         rating: { type: GraphQLFloat },
         classification: { type: GraphQLString },
-        year: { type: GraphQLString }
-        // Tarea 3 - Añadir el campo 'directors' y devolver la información correspondiente.
-        // Tarea 4 - Añadir el campo 'actors' y devolver la información correspondiente.
+        year: { type: GraphQLString },
+        directors: {
+            type: new GraphQLList(DirectorType),
+            resolve: async (parentValues, args) => {
+                return await MoviesDirectorsService.getDirectorsDataByMovieId(parentValues.id);
+            }
+        },
+        writers: {
+            type: new GraphQLList(WriterType),
+            resolve: async (parentValues, args) => {
+                return await MoviesWritersService.getWritersDataByMovieId(parentValues.id);
+            }
+        },
+        actors: {
+            type: new GraphQLList(ActorType),
+            resolve: async (parentValues, args) => {
+                return await MoviesActorsService.getActorsDataByMovieId(parentValues.id);
+            }
+        },
+        genres: {
+            type: new GraphQLList(GenreType),
+            resolve: async (parentValues, args) => {
+                return await MoviesGenresService.getGenresDataByMovieId(parentValues.id);
+            }
+        },
+        genresAsArray: {
+            type: new GraphQLList(GraphQLString),
+            resolve: async (parentValues, args) => {
+                let genres = await MoviesGenresService.getGenresDataByMovieId(parentValues.id);
+                return genres.map(genre => genre.name);
+            }
+        }
     })
 });
 
 export default MovieType;
 
-
-/*
-Solución Tarea 1.a
-import * as MoviesDirectorsService from '../services/movies-directors.service';
-Solución Tarea 1.b
 import DirectorType from './director.type';
-
-Solución Tarea 2.a
-import * as MoviesActorsService from '../services/movies-actors.service';
-Solución Tarea 2.b
+import WriterType from './writer.type';
 import ActorType from './actor.type';
-
-Solución Tarea 3
-directors: {
-    type: new GraphQLList(DirectorType),
-    resolve: async (parentValues, args) => {
-        return await MoviesDirectorsService.getDirectorsDataByMovieId(parentValues.id);
-    }
-},
-
-Solución Tarea 4
-actors: {
-    type: new GraphQLList(ActorType),
-    resolve: async (parentValues, args) => {
-        return await MoviesActorsService.getActorsDataByMovieId(parentValues.id);
-    }
-},
-*/
+import GenreType from './genre.type';
